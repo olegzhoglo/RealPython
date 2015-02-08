93db5e34e6e0d3ac794f30f4aba316043e61505b
Oleg Zhoglo <oleg@Olegs-MacBook-Air.local>
2015-02-08 15:49:28 -0800

    final version of the capital_city_loop assignment

--- 9/capital_city_loop.py
+++ 9/capital_city_loop.py
@@ -1,4 +1,30 @@
 from capitals import capitals_dict
 import random
 
-print capitals_dict
+def getCapital():
+	keyList = capitals_dict.keys() #grab entire list of keys
+	state = random.choice(keyList) #select random key
+	capital = capitals_dict[state] #get capitol
+
+	return state, capital
+
+def userInteraction():
+	userInput = ""
+	while userInput != "exit":
+		stateCapitalPair = getCapital()
+		state = stateCapitalPair[0]
+		capital = stateCapitalPair[1]
+		answer = raw_input("What is the capital of " + str(state) + "? ")
+		if answer == "exit" or answer == "Exit" or answer == "EXIT":
+			break
+		elif answer == capital or answer == str.lower(capital):
+			print("Correct!")
+		else:
+			print("Wrong!")
+
+
+userInteraction()
+
+
+
+
