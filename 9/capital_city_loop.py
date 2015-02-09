3512135cd342e1936d167e896c2b72c01266472e
Oleg Zhoglo <oleg@Olegs-MacBook-Air.local>
2015-02-08 16:13:44 -0800

    made more efficient by removing unecessary keyList variable

--- 9/capital_city_loop.py
+++ 9/capital_city_loop.py
@@ -2,15 +2,14 @@ from capitals import capitals_dict
 import random
 
 def getCapital():
-	keyList = capitals_dict.keys() #grab entire list of keys
-	state = random.choice(keyList) #select random key
+	state = random.choice(capitals_dict.keys()) #select random key
 	capital = capitals_dict[state] #get capitol
 
 	return state, capital
 
 def userInteraction():
 	userInput = ""
-	while userInput != "exit":
+	while True:
 		stateCapitalPair = getCapital()
 		state = stateCapitalPair[0]
 		capital = stateCapitalPair[1]
@@ -21,7 +20,6 @@ def userInteraction():
 			print("Correct!")
 		else:
 			print("Wrong!")
-
 
 userInteraction()
 
